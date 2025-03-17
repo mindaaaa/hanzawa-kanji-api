360267316bd85dee547e0fd599b0d0246da06d3f
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-03-17 19:40:59 +0900

    init: 프로젝트 스캐폴딩

---
+++ src/main/kotlin/com/mindaaaa/hanzawakanji/Application.kt
@@ -0,0 +1,11 @@
+package com.mindaaaa.hanzawakanji
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HanazawakanjiApplication
+
+fun main(args: Array<String>) {
+	runApplication<HanazawakanjiApplication>(*args)
+}
---
+++ src/test/kotlin/com/mindaaaa/hanzawakanji/HanazawakanjiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mindaaaa.hanzawakanji
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HanazawakanjiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
