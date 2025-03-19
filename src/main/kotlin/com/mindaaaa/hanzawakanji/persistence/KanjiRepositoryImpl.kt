bac9142d461ae7de0456b5cd6d8375a09bcff05a
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-03-19 19:13:45 +0900

    chore: 리포지토리 개발 계획 주석 작성

--- src/main/kotlin/com/mindaaaa/hanzawakanji/persistence/KanjiRepositoryImpl.kt
+++ src/main/kotlin/com/mindaaaa/hanzawakanji/persistence/KanjiRepositoryImpl.kt
@@ -9,5 +9,10 @@ class KanjiRepositoryImpl(
 ) : KanjiRepository {
     override fun list(mode: Mode, limit: Int, cursor: String?): List<Kanji> {
         TODO("Not yet implemented")
+        // 1. dataSource로부터 한자 목록을 가져온다
+
+        // 2. mode에 따라 섞거나 섞지 않는다
+
+        // 3. cursor와 limit에 따라 2.의 결과를 잘라 반환한다.
     }
 }
