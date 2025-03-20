bb7f4e3dc042979a4df007e490c91e5f0e745af2
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-03-20 19:21:27 +0900

    feat: json 파일 작성 및 모델 타입 업데이트

--- src/main/kotlin/com/mindaaaa/hanzawakanji/db/model/Kanji.kt
+++ src/main/kotlin/com/mindaaaa/hanzawakanji/db/model/Kanji.kt
@@ -3,9 +3,13 @@ package com.mindaaaa.hanzawakanji.db.model
 data class Kanji(
     val id: Int,
     val value: String,
-    val reading: String,
-    val meaning: String,
-    val onyomi: List<String>,
+    val korean: List<Reading>,
     val kunyomi: List<String>,
+    val onyomi: List<String>,
     val traditionalForm: String?,
-)
+) {
+    data class Reading(
+        val kun: String,
+        val on: String,
+    )
+}
