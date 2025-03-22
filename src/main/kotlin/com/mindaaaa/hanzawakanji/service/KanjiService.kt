daf3f97ce61b302cc5c03c4191cefadfc515b7b3
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-03-22 18:59:05 +0900

    feat: 서비스 구현체 작성

---
+++ src/main/kotlin/com/mindaaaa/hanzawakanji/service/KanjiService.kt
@@ -0,0 +1,40 @@
+package com.mindaaaa.hanzawakanji.service
+
+import com.mindaaaa.hanzawakanji.persistence.KanjiRepository
+import com.mindaaaa.hanzawakanji.persistence.model.Mode
+import com.mindaaaa.hanzawakanji.service.dto.ListRequestDto
+import com.mindaaaa.hanzawakanji.service.dto.ListResponseDto
+import org.springframework.stereotype.Service
+
+@Service
+class KanjiService(
+    private val repository: KanjiRepository,
+) {
+    companion object {
+        private val DEFAULT_MODE: Mode = Mode.NORMAL
+        private const val DEFAULT_LIMIT: Int = 10
+        private const val DEFAULT_CURSOR: Int = 1
+    }
+
+    fun list(dto: ListRequestDto): ListResponseDto {
+        val limit = dto.limit ?: DEFAULT_LIMIT
+
+        val refinedLimit = limit + 1
+        val kanjiList = repository.list(
+            limit = refinedLimit,
+            quizId = dto.quizId,
+            mode = dto.mode ?: DEFAULT_MODE,
+            cursor = dto.cursor ?: DEFAULT_CURSOR,
+        )
+        val totalCount = repository.getTotalCount()
+
+        return if (kanjiList.size == refinedLimit) {
+            val lastOne = kanjiList.last()
+            val items = kanjiList.take(limit)
+
+            ListResponseDto(items, lastOne.id, totalCount)
+        } else {
+            ListResponseDto(kanjiList, null, totalCount)
+        }
+    }
+}
