3a6542f37aa2ccf1655fde1681f8adfe851ffb38
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-03-22 19:22:39 +0900

    feat: CORS 설정 적용

---
+++ src/main/kotlin/com/mindaaaa/hanzawakanji/presentation/WebConfiguration.kt
@@ -0,0 +1,18 @@
+package com.mindaaaa.hanzawakanji.presentation
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfiguration : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+
+        super.addCorsMappings(registry)
+    }
+}
