38dbf280f5561618a1c41838ed2c45c147324bcc
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-03-22 14:50:10 +0900

    test: IndexOutOfBoundsException 처리에 관한 테스트 코드 작성

--- src/test/kotlin/com/mindaaaa/hanzawakanji/persistence/KanjiRepositoryImplTest.kt
+++ src/test/kotlin/com/mindaaaa/hanzawakanji/persistence/KanjiRepositoryImplTest.kt
@@ -2,54 +2,72 @@ package com.mindaaaa.hanzawakanji.persistence
 
 import com.mindaaaa.hanzawakanji.db.KanjiDataSource
 import com.mindaaaa.hanzawakanji.persistence.model.Mode
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 
-class KanjiRepositoryImplTest : StringSpec({
-    "list() 메소드는 mode 값에 따라 섞이거나 섞이지 않은 Kanji 리스트를 반환한다." {
-        // given
-        val limit = 10
-        val kanjiRepository: KanjiRepository = KanjiRepositoryImpl(
-            dataSource = KanjiDataSource(),
-        )
+class KanjiRepositoryImplTest : DescribeSpec({
+    describe("list() 메소드 테스트") {
+        it("mode 값에 따라 섞이거나 섞이지 않은 Kanji 리스트를 반환한다.") {
+            // given
+            val limit = 10
+            val kanjiRepository: KanjiRepository = KanjiRepositoryImpl(
+                dataSource = KanjiDataSource(),
+            )
 
-        // when
-        val normalList = kanjiRepository.list(Mode.NORMAL, limit, null, null)
-        val shuffledList = kanjiRepository.list(Mode.RANDOM, limit, null, null)
+            // when
+            val normalList = kanjiRepository.list(Mode.NORMAL, limit, null, null)
+            val shuffledList = kanjiRepository.list(Mode.RANDOM, limit, null, null)
 
-        // then
-        normalList shouldNotBe shuffledList
-    }
+            // then
+            normalList shouldNotBe shuffledList
+        }
 
-    "list() 메소드에 RANDOM 모드를 전달한 경우, 인자에 전달된 id에 따라 항상 동일한 결과를 반환할 수 있어야 한다." {
-        // given
-        val id = "hello world"
-        val limit = 10
-        val kanjiRepository: KanjiRepository = KanjiRepositoryImpl(
-            dataSource = KanjiDataSource(),
-        )
+        it("RANDOM 모드를 전달한 경우, 인자에 전달된 id에 따라 항상 동일한 결과를 반환할 수 있어야 한다.") {
+            // given
+            val id = "hello world"
+            val limit = 10
+            val kanjiRepository: KanjiRepository = KanjiRepositoryImpl(
+                dataSource = KanjiDataSource(),
+            )
 
-        // when
-        val first = kanjiRepository.list(Mode.RANDOM, limit, null, id)
-        val second = kanjiRepository.list(Mode.RANDOM, limit, null, id)
+            // when
+            val first = kanjiRepository.list(Mode.RANDOM, limit, null, id)
+            val second = kanjiRepository.list(Mode.RANDOM, limit, null, id)
 
-        // then
-        first shouldBe second
-    }
+            // then
+            first shouldBe second
+        }
 
-    "list() 메소드에서 cursor에 해당하는 Kanji가 없다면 빈 리스트를 반환한다." {
-        // given
-        val cursor = 99999
-        val limit = 10
-        val kanjiRepository: KanjiRepository = KanjiRepositoryImpl(
-            dataSource = KanjiDataSource(),
-        )
+        it("cursor에 해당하는 Kanji가 없다면 빈 리스트를 반환한다.") {
+            // given
+            val cursor = 99999
+            val limit = 10
+            val kanjiRepository: KanjiRepository = KanjiRepositoryImpl(
+                dataSource = KanjiDataSource(),
+            )
 
-        // when
-        val list = kanjiRepository.list(Mode.NORMAL, limit, cursor, null)
+            // when
+            val list = kanjiRepository.list(Mode.NORMAL, limit, cursor, null)
 
-        // then
-        list shouldBe emptyList()
+            // then
+            list shouldBe emptyList()
+        }
+
+        it("IndexOutOfBoundsException이 발생하지 않는다.") {
+            // given
+            val cursor = 2100
+            val limit = 40
+            val kanjiRepository: KanjiRepository = KanjiRepositoryImpl(
+                dataSource = KanjiDataSource(),
+            )
+
+            // when
+            // then
+            shouldNotThrow<IndexOutOfBoundsException> {
+                kanjiRepository.list(Mode.NORMAL, limit, cursor, null)
+            }
+        }
     }
 })
